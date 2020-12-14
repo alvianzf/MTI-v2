0b022ee46a6fe508862fbb466101ded3db0369f5
Alvian Z Faturrahman <alvianzfaturrahman@iMaCs-iMac.local>
2020-12-14 19:47:18 +0700

    Change date test case

--- whenWasItPosted/index.js
+++ whenWasItPosted/index.js
@@ -5,10 +5,18 @@ const whenWasItPosted = date => {
 
 
 // do not change this code below
+const date = new Date();
+const changeToString = __ => {
+  const _ = new Date(__)
+  const month = _.toLocaleString('default', { month: 'long' });
+  const date = _.getDate()
+  const year = _.getFullYear()
+  return `${date} ${month} ${year}`
+}
 const test = (testCase, result) => console.log(testCase === result);
 
-test(whenWasItPosted("2 January 2020"), '11 months ago')
-test(whenWasItPosted("25 November 2020"), '2 weeks ago')
-test(whenWasItPosted("10 December 2020"), '3 days ago')
-test(whenWasItPosted("1 December 2019"), '1 year ago')
-test(whenWasItPosted("23 October 2000"), '20 years ago')
+test(whenWasItPosted(changeToString(date.setMonth(date.getMonth() - 11))), '11 months ago')
+test(whenWasItPosted(changeToString(date.setDate(date.getDate() - 14))), '2 weeks ago')
+test(whenWasItPosted(changeToString(date.setDate(date.getDate() - 3))), '3 days ago')
+test(whenWasItPosted(changeToString(date.setFullYear(date.getFullYear() - 1))), '1 year ago')
+test(whenWasItPosted(changeToString(date.setFullYear(date.getFullYear() - 20))), '20 years ago')

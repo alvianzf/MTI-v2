beec89597d0577b9372091d171bf58ca5b099554
Alvianzf <alvianzf@gmail.com>
2020-12-14 20:17:47 +0700

    fix

--- whenWasItPosted/index.js
+++ whenWasItPosted/index.js
@@ -5,7 +5,6 @@ const whenWasItPosted = date => {
 
 
 // do not change this code below
-const date = new Date();
 const changeToString = __ => {
   const _ = new Date(__)
   const month = _.toLocaleString('default', { month: 'long' });
@@ -15,8 +14,14 @@ const changeToString = __ => {
 }
 const test = (testCase, result) => console.log(testCase === result);
 
+const date = new Date();
+const date1 = new Date();
+const date2 = new Date();
+const date3 = new Date();
+const date4 = new Date();
+
 test(whenWasItPosted(changeToString(date.setMonth(date.getMonth() - 11))), '11 months ago')
-test(whenWasItPosted(changeToString(date.setDate(date.getDate() - 14))), '2 weeks ago')
-test(whenWasItPosted(changeToString(date.setDate(date.getDate() - 3))), '3 days ago')
-test(whenWasItPosted(changeToString(date.setFullYear(date.getFullYear() - 1))), '1 year ago')
-test(whenWasItPosted(changeToString(date.setFullYear(date.getFullYear() - 20))), '20 years ago')
+test(whenWasItPosted(changeToString(date1.setDate(date.getDate() - 14))), '2 weeks ago')
+test(whenWasItPosted(changeToString(date2.setDate(date.getDate() - 3))), '3 days ago')
+test(whenWasItPosted(changeToString(date3.setFullYear(date.getFullYear() - 1))), '1 year ago')
+test(whenWasItPosted(changeToString(date4.setFullYear(date.getFullYear() - 20))), '20 years ago')

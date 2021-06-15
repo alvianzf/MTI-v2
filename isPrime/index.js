7d0dccbd9891c7755209701754944d0b987b2fe7
alvianzf <alvianzf@gmail.com>
2021-06-15 23:09:52 +0700

    test case

--- isPrime/index.js
+++ isPrime/index.js
@@ -1,5 +1,22 @@
 function isPrime(num) {
   // do code here
+  var n, i, flag = true;
+              
+  // Getting the value form text
+  // field using DOM
+  n = document.myform.n.value;
+  n = parseInt(n)
+  for(i = 2; i <= n - 1; i++)
+      if (n % i == 0) {
+          flag = false;
+          break;
+      }
+        
+      // Check and display alert message
+  if (flag == true)
+      return n + " IS A PRIME";
+  else
+      return n + " IS NOT A PRIME";
   return num
 }
 

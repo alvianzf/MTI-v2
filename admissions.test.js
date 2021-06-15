f428e9d2d26775454f9a1008640d63b5c1c35150
alvianzf <alvianzf@gmail.com>
2021-06-15 23:21:19 +0700

    remove guiding question

--- admissions.test.js
+++ admissions.test.js
@@ -6,14 +6,9 @@ const isPrime = require('./isPrime');
 // });
 
 test('Is it prime ?', () => {
-  console.log("2")
   expect(isPrime(2)).toBe('2 IS A PRIME')
-  console.log("389")
   expect(isPrime(389)).toBe('389 IS A PRIME')
-  console.log("973")
   expect(isPrime(973)).toBe('973 IS NOT A PRIME')
-  console.log("283")
   expect(isPrime(283)).toBe('283 IS A PRIME')
-  console.log("21")
   expect(isPrime(21)).toBe('21 IS NOT A PRIME')
 })

b85def226bd9702076b30915e8776239643bd4e9
alvianzf <alvianzf@gmail.com>
2021-06-15 23:05:36 +0700

    update test

--- admissions.test.js
+++ admissions.test.js
@@ -6,16 +6,16 @@ const isPrime = require('./isPrime');
 // });
 
 test('Is 2 prime ?', () => {
-  expect(isPrime(2).toBe('2 IS A PRIME'))
+  expect(isPrime(2)).toBe('2 IS A PRIME')
 })
 
 test('Is 283 prime ?', () => {
-  expect(isPrime(2).toBe('283 IS A PRIME'))
+  expect(isPrime(283)).toBe('283 IS A PRIME')
 })
 test('Is 21 prime ?', () => {
-  expect(isPrime(21).toBe('21 IS NOT A PRIME'))
+  expect(isPrime(21)).toBe('21 IS NOT A PRIME')
 })
 test('Is 389 prime ?', () => {
-  expect(isPrime(389).toBe('389 IS A PRIME'))
-  expect(isPrime(973).toBe('973 IS NOT A PRIME'))
+  expect(isPrime(389)).toBe('389 IS A PRIME')
+  expect(isPrime(973)).toBe('973 IS NOT A PRIME')
 })

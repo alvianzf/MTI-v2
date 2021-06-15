5fafeec20f918c8aee77659ad8d6ce5ef7cbd440
alvianzf <alvianzf@gmail.com>
2021-06-15 23:56:13 +0700

    when was it posted test

--- .github/tests/whenWasItPosted.test.js
+++ .github/tests/whenWasItPosted.test.js
@@ -11,6 +11,14 @@ test('whenWasItPosted?', () => {
     expect(whenWasItPosted(changeToString(date2.setDate(date.getDate() - 3)))).toBe('3 days ago');
     expect(whenWasItPosted(changeToString(date3.setFullYear(date.getFullYear() - 1)))).toBe('1 year ago');
     expect(whenWasItPosted(changeToString(date4.setFullYear(date.getFullYear() - 20)))).toBe('20 years ago');
-
 })
+
+// do not change this code below
+const changeToString = __ => {
+    const _ = new Date(__)
+    const month = _.toLocaleString('default', { month: 'long' });
+    const date = _.getDate()
+    const year = _.getFullYear()
+    return `${date} ${month} ${year}`
+  }
 

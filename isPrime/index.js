af3d2acf839804ab9fa543407c7a8b6bb00cffc0
alvianzf <alvianzf@gmail.com>
2021-06-15 23:24:37 +0700

    what time is it

--- isPrime/index.js
+++ isPrime/index.js
@@ -1,20 +1,6 @@
 function isPrime(num) {
   // do code here
-  var flag = true;
-              
-  // Getting the value form text
-  // field using DOM
-  for(i = 2; i <= num - 1; i++)
-      if (num % i == 0) {
-          flag = false;
-          break;
-      }
-        
-      // Check and display alert message
-  if (flag == true)
-      return num + " IS A PRIME";
-  else
-      return num + " IS NOT A PRIME";
+  return "It's noon"
 }
 
 module.exports = isPrime;
